12a249ea83cfcab68bc2386b689abc5b29fab18e
mathlearn <famoralearn@gmail.com>
2026-04-17 16:49:01 +0700

    update logo title

--- cari-teman/cari-teman.php
+++ cari-teman/cari-teman.php
@@ -44,7 +44,7 @@ $kecamatan_list = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cari Teman - FamoraLearn</title>
-    <link rel="icon" type="image/jpeg" href="../assets/images/logo.jpeg">
+    <link rel="icon" type="image/png" href="../assets/images/famora.png">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
